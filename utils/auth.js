d0fccdea942d10ec06a27f64448947a335caa8a3
Alex Vega <alexvega@Alexs-MBP.lan>
2023-06-29 14:35:53 -0400

    adds withAuth util

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,11 @@
-module.exports = (req, res, next) => {
-    if(req.session.loggedIn) {
-        return next()
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
     }
-    res.redirect('/login');
-}
+  };
+  
+  module.exports = withAuth;
+  
