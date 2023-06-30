a297fd71f2ed850c6d2b77a79107983ec5dbe734
Chris <christopherlee2477@gmail.com>
2023-06-30 10:57:23 -0400

    added the querySelectors into the profile js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,9 @@
+const sumbitForm = async (event) => {
+  event.preventDefault();
 
+  const name = document.querySelector('#chart-name').value.trim();
+  const dateOfBirth = document.querySelector('#date-birth').value.trim();
+  const place = document.querySelector('#place-birth').value.trim();
+  const time = document.querySelector('#time-birth').value.trim();
+  // const gender = document.querySelector('#gender').value.trim();
+};
