94973826d3ea54d59cdc6cc145a778f90b78bc70
Tawhid <tawhidkamal5@gmail.com>
2023-07-05 15:17:52 -0400

    horoscopes and routes suffering

--- controller/api/horoscopes.js
+++ controller/api/horoscopes.js
@@ -1,16 +1,22 @@
+const router = require('express').Router();
+
 require('dotenv').config();
 // 2: handle request from your frontend
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // 3: get data from third party api
     const options = {
         method: 'GET',
-        url: 'https://horoscope34.p.rapidapi.com/api/horoscope/today',
         headers: {
           'X-RapidAPI-Key': process.env.API_KEY,
           'X-RapidAPI-Host': 'horoscope34.p.rapidapi.com',
         },
       };
-      res.json(options);
+      const response = await fetch('https://horoscope34.p.rapidapi.com/api/horoscope/today', options);
+      const horoscopes = await response.json();
+      console.log(horoscopes)
+      res.json(horoscopes);
 
     // 4: respond to your front end with data
 })
+
+module.exports = router;
